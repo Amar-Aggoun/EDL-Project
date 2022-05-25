842ecfebbc7991cea38aaaca883d42fcf162e9d7
Amar Aggoun <amar.aggoun@univ-constantine2.dz>
2022-05-25 20:08:06 +0100

    Add files via upload

---
+++ Implementation/edl/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'EDL',
+    'version': '1.0.0',
+    'category': 'Education',
+    'author': 'M1GL Dev team',
+    'summary': 'Transfer requests form',
+    'description': """Form for students to submit their transfer requests""",
+    'sequence': -100,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+}

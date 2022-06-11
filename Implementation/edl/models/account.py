2d022f6f6ae7d046e9762cf269697faa536fe07a
Amar-Aggoun <amar.aggoun@univ-constantine2.dz>
2022-06-11 18:20:22 +0100

    added email, password, account_type fields to transfer_account table

--- Implementation/edl/models/account.py
+++ Implementation/edl/models/account.py
@@ -1,5 +1,9 @@
 from odoo import api, models, fields
 
-class CandidatAccounts(models.Model):
-    _name = "candidat.account"
-    _description = "candidat accounts"
+class Accounts(models.Model):
+    _name = "transfer.account"
+    _description = "transfer accounts"
+
+    email = fields.Char(String = "Email")
+    password = fields.Char(String = "Password")
+    account_type = fields.Selection([("candidat", "Candidat"), ("agent", "Agent")], String = "Type")
